10280cfec74d12fc445df1e9fb26a53342e7910c
Laptop <memorosas.00@gmail.com>
2025-01-17 22:35:37 -0600

    en proceso, leccion 155

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //  aqui se define las rutas de la htmls
+  // 01:07
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
