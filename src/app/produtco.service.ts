810913e4df563f5a1500c080c6e9f54953a9f86a
Laptop <memorosas.00@gmail.com>
2025-01-18 12:13:27 -0600

    finaliza, leccion 157
    se agrega el servicio para consultar al bakend la lista.

--- src/app/produtco.service.ts
+++ src/app/produtco.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from './producto';
 
+// injhectable necvesaria para usar el servicio desde comonete producto lista
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutcoService {
+  private urlBase = "http://localhost:8080/inventario-app/productos";
+  constructor(private clienteHttp: HttpClient) { }
 
-  constructor() { }
+    // : esto es lo que regresa, observable se importa desde rxjs
+    // producto se importo desde ./produto con ayuda rapida
+    obtenerProductosLista(): Observable<Producto []> {
+      // (esto es un parametro)
+      return this.clienteHttp.get<Producto[]>(this.urlBase);
+
+
+    }
 }
